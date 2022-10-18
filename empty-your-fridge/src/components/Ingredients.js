98eef629f6b52ddfbcd8c00a3cc22e66a8c4f586
Ben Smerd <bsmerd@adam.com.au>
2022-10-18 22:01:22 +1030

    added in ingredients

--- empty-your-fridge/src/components/Ingredients.js
+++ empty-your-fridge/src/components/Ingredients.js
@@ -2,3 +2,19 @@
 // var api_key = '3825b049c21e45afc20c029947cc4427'
 
 // apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${queryIngredients}&app_id=${api_id}&app_key=${api_key}&diet=high-fiber${cuisine}${meal}`
+
+import React from "react";
+
+const Ingredients = () => {
+  return (
+    <div className="ingredients-container">
+      <h3>
+        Type in ingredients you have laying about, one by one to then receive a
+        list of yummy recipes that you can make with them. It's time to empty
+        your fridge!
+      </h3>
+    </div>
+  );
+};
+
+export default Ingredients;
--- empty-your-fridge/src/index.js
+++ empty-your-fridge/src/index.js
@@ -4,9 +4,4 @@ import "./styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div>
-    <h1>HEY</h1>
-    <p>HEYYY</p>
-  </div>
-);
+root.render(<App />);
