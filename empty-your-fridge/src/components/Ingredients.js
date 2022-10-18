ce2103c390bf18dffdca9247634a67e4e9eaf7f0
Ben Smerd <bsmerd@adam.com.au>
2022-10-18 23:01:12 +1030

    added in form for Ingredients component with styling

--- empty-your-fridge/src/components/Ingredients.js
+++ empty-your-fridge/src/components/Ingredients.js
@@ -1,9 +1,10 @@
 // var api_id = '215db5b9'
 // var api_key = '3825b049c21e45afc20c029947cc4427'
 
-// apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${queryIngredients}&app_id=${api_id}&app_key=${api_key}&diet=high-fiber${cuisine}${meal}`
-
+// apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${queryIngredients}&app_id=${api_id}&app_key=${api_key}${health}${cuisine}${meal}`
 import React from "react";
+import { cuisines, meals, health } from "../utils/types";
+import "../styles/Ingredients.css";
 
 const Ingredients = () => {
   return (
@@ -13,6 +14,40 @@ const Ingredients = () => {
         list of yummy recipes that you can make with them. It's time to empty
         your fridge!
       </h3>
+      <form className="form-container">
+        <input
+          type="text"
+          className="ingredient-input"
+          placeholder="Ingredients"
+        />
+        <select className="cuisine-input">
+          <option value="" selected disabled hidden>
+            Cuisine type
+          </option>
+          {cuisines.map((cuisine) => (
+            <option value={cuisine.toLowerCase()}>{cuisine}</option>
+          ))}
+        </select>
+        <select className="meal-input">
+          <option value="" selected disabled hidden>
+            Meal type
+          </option>
+          {meals.map((meal) => (
+            <option value={meal.toLowerCase()}>{meal}</option>
+          ))}
+        </select>
+        <select className="health-type">
+          <option value="" selected disabled hidden>
+            Health type
+          </option>
+          {health.map((type) => (
+            <option value={type.toLowerCase()}>{type}</option>
+          ))}
+        </select>
+        <button type="submit" className="submit-input">
+          Search recipes
+        </button>
+      </form>
     </div>
   );
 };
