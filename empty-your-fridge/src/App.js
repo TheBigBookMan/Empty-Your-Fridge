3ea2fbfc75743d793f6a3906fbe1a0818ad4532c
Ben Smerd <bsmerd@adam.com.au>
2022-10-18 22:01:14 +1030

    added App component and style for header

--- empty-your-fridge/src/App.js
+++ empty-your-fridge/src/App.js
@@ -1,11 +1,15 @@
 import "./styles/App.css";
+import Ingredients from "./components/Ingredients";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <h1>HEYYY</h1>
+        <h1>Empty Your Fridge</h1>
       </header>
+      <body className="container">
+        <Ingredients />
+      </body>
     </div>
   );
 }
