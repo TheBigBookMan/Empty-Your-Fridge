b2ce7aa8bc09d8cf6b3084d5b146a04a71d43915
Ben Smerd <bsmerd@adam.com.au>
2022-10-18 18:10:39 +1030

    added component files

---
+++ empty-your-fridge/src/components/Ingredients.js
@@ -0,0 +1,4 @@
+// var api_id = '215db5b9'
+// var api_key = '3825b049c21e45afc20c029947cc4427'
+
+// apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${queryIngredients}&app_id=${api_id}&app_key=${api_key}&diet=high-fiber${cuisine}${meal}`
