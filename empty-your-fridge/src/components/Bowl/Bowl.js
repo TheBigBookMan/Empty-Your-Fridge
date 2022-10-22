3abf0f0e415f40e057f06b8ab4841c6d944a2f9a
Ben Smerd <bsmerd@adam.com.au>
2022-10-22 13:27:24 +1030

    adding in the list of ingredients added in by user

--- empty-your-fridge/src/components/Bowl/Bowl.js
+++ empty-your-fridge/src/components/Bowl/Bowl.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles/Bowl.css";
 import { bowlStyles as bowl } from "./styles/BowlStyles";
 
 // need to import the state array from ingredients to then have list on on here
@@ -7,9 +8,58 @@ const Bowl = () => {
   return (
     <div style={bowl.bowlContainer}>
       <ul style={bowl.bowlList}>
-        <li>Ham</li>
-        <li>Cheese</li>
-        <li>Lettuce</li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>cheese</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>lettuce</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Hamchickencheesesam</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
+        <li style={bowl.ingredient}>
+          <p style={bowl.ingredientName}>Ham</p>{" "}
+          <i class="fa-regular fa-circle-xmark"></i>
+        </li>
       </ul>
     </div>
   );
--- empty-your-fridge/src/components/Bowl/styles/BowlStyles.js
+++ empty-your-fridge/src/components/Bowl/styles/BowlStyles.js
@@ -1,9 +1,10 @@
 export const bowlStyles = {
   bowlContainer: {
     border: "black solid 2px",
-    height: "200px",
-    width: "400px",
+    maxHeight: "150px",
+    width: "800px",
     fontSize: "1.3rem",
+    marginTop: "15px",
   },
   bowlList: {
     listStyleType: "none",
@@ -12,5 +13,17 @@ export const bowlStyles = {
     flexWrap: "wrap",
     gap: "10px",
     fontWeight: "bolder",
+    maxHeight: "150px",
+    width: "800px",
+  },
+  ingredient: {
+    display: "flex",
+    maxWidth: "auto",
+  },
+  ingredientName: {
+    marginRight: "5px",
+    marginTop: "-5px",
+    maxWidth: "150px",
+    wordWrap: "break-word",
   },
 };
