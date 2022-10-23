4dd70e738b401b35c1886960683f49670f26152f
Ben Smerd <bsmerd@adam.com.au>
2022-10-24 10:03:06 +1030

    updated query algorithm

--- empty-your-fridge/src/utils/API.js
+++ empty-your-fridge/src/utils/API.js
@@ -3,32 +3,53 @@ import axios from "axios";
 var api_id = "215db5b9";
 var api_key = "3825b049c21e45afc20c029947cc4427";
 
+const ingredientCheck = (arr) => {
+  let ingredientSelection = [arr[0]];
+  if (arr.length < 1) {
+    return;
+  } else {
+    for (let i = 1; i < arr.length; i++) {
+      ingredientSelection.push(`%${arr[i]}`);
+    }
+  }
+  return ingredientSelection.join("");
+};
+
+const healthCheck = (input) => {
+  if (input === "" || input === "any" || input === "health type") {
+    return "";
+  } else {
+    return `&health=${input}`;
+  }
+};
+
+const cuisineCheck = (input) => {
+  if (input === "" || input === "any" || input === "select cuisine") {
+    return "";
+  } else if (input.split(" ").length > 1) {
+    return `&cuisineType=${input.split(" ").join("%20")}`;
+  } else {
+    return `&cuisineType=${input}`;
+  }
+};
+
+const mealCheck = (input) => {
+  if (input === "" || input === "any" || input === "meal type") {
+    return "";
+  } else {
+    return `&mealType=${input}`;
+  }
+};
+
 const apiQuery = async (queryIngredients, health, cuisine, meal) => {
   // ? need to make the api call like this with the queries so need if/else statements
   //? https://api.edamam.com/api/recipes/v2?type=public&q=lettuce%chicken%tomato%cheese&app_id=215db5b9&app_key=3825b049c21e45afc20c029947cc4427&health=alcohol-free&cuisineType=British&mealType=Lunch
 
-  let ingredientSelection = [queryIngredients[0]];
-  if (queryIngredients.length < 1) {
-    return;
-  } else {
-    for (let i = 1; i < queryIngredients.length; i++) {
-      ingredientSelection.push(`%${queryIngredients[i]}`);
-    }
-  }
-  ingredientSelection = ingredientSelection.join("");
-
-  // TODO update the health variable to '&health={health}' and if no input then ""
-  if (health === "" || health === "Any") {
-    health = "";
-  } else {
-    health = `&health=${health}`;
-  }
-  console.log(health);
-  // TODO update the cuisine variable to '&cuisineType={cuisine}' and if no input then ""
-
-  // TODO update the meal variable to '&mealType={meal}' and if no input then ""
-
-  var apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredientSelection}&app_id=${api_id}&app_key=${api_key}${health}${cuisine}${meal}`;
+  var apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredientCheck(
+    queryIngredients
+  )}&app_id=${api_id}&app_key=${api_key}${healthCheck(health)}${cuisineCheck(
+    cuisine
+  )}${mealCheck(meal)}`;
   console.log(apiUrl);
   // const response = await axios.get(apiUrl);
   // console.log(response);
