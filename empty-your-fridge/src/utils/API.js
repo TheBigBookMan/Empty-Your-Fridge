ba35052e1d20ebbaa3d97cad165f797a99e58f9a
Ben Smerd <bsmerd@adam.com.au>
2022-10-23 19:49:03 +1030

    added the api call url to food API

---
+++ empty-your-fridge/src/utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+var api_id = "215db5b9";
+var api_key = "3825b049c21e45afc20c029947cc4427";
+
+const apiQuery = async (queryIngredients, health, cuisine, meal) => {
+  var apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${queryIngredients}&app_id=${api_id}&app_key=${api_key}${health}${cuisine}${meal}`;
+
+  const response = await axios.get(apiUrl);
+  console.log(response);
+};
+
+export default apiQuery;
