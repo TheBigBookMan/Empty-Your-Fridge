1e726790e0c0aa2fe343b41205a261221dfe091c
Ben Smerd <bsmerd@adam.com.au>
2022-11-08 16:44:45 +1030

    added in remove ingredient function to the Bowl

--- empty-your-fridge/src/components/Bowl/Bowl.js
+++ empty-your-fridge/src/components/Bowl/Bowl.js
@@ -6,14 +6,17 @@ import { bowlStyles as bowl } from "./styles/BowlStyles";
 //TODO need to make the X buttons highlight red on hover and then onClick will remove the ingredients from the ingredients arr
 
 const Bowl = () => {
-  const { ingredients } = useIngredientContext();
+  const { ingredients, removeIngredient } = useIngredientContext();
   return (
     <div style={bowl.bowlContainer}>
       <ul style={bowl.bowlList}>
         {ingredients.map((ingredient) => (
           <li style={bowl.ingredient} key={ingredient} value={ingredient}>
             <p style={bowl.ingredientName}>{ingredient.toLowerCase()}</p>{" "}
-            <i className="fa-regular fa-circle-xmark cross"></i>
+            <i
+              className="fa-regular fa-circle-xmark cross"
+              onClick={() => removeIngredient(ingredient)}
+            ></i>
           </li>
         ))}
       </ul>
