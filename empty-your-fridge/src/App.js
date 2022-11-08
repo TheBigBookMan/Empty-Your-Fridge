f7ff4a9c607bd151b457bc232f944045c05a80c9
Ben Smerd <bsmerd@adam.com.au>
2022-11-08 12:40:08 +1030

    added the ingredient provider wrapper around the children components

--- empty-your-fridge/src/App.js
+++ empty-your-fridge/src/App.js
@@ -1,11 +1,13 @@
 import "./styles/App.css";
 import { useState } from "react";
+import { IngredientProvider } from "./utils/IngredientsContext";
 import Ingredients from "./components/Ingredients/Ingredients";
 import Bowl from "./components/Bowl/Bowl";
 import Recipes from "./components/Recipes/Recipes";
 import Favourites from "./components/Favourites/Favourites";
 
 function App() {
+  // ! can probably delete the usestate for ingredients because now in global state
   const [ingredientsArr, setIngredientsArr] = useState([]);
   const [recipesInfoArr, setRecipesInfoArr] = useState([]);
 
@@ -20,22 +22,24 @@ function App() {
         <h1>Empty Your Fridge</h1>
       </header>
       <main className="container">
-        <Ingredients
-          setIngredientsArr={setIngredientsArr}
-          ingredientsArr={ingredientsArr}
-          recipesInfoArr={recipesInfoArr}
-          setRecipesInfoArr={setRecipesInfoArr}
-        />
-        <div className="middle-container">
-          <Bowl
-            ingredientsArr={ingredientsArr}
+        <IngredientProvider>
+          <Ingredients
             setIngredientsArr={setIngredientsArr}
+            ingredientsArr={ingredientsArr}
+            recipesInfoArr={recipesInfoArr}
+            setRecipesInfoArr={setRecipesInfoArr}
           />
-          <Recipes recipesInfoArr={recipesInfoArr} />
-        </div>
-        <div className="right-container">
-          <Favourites />
-        </div>
+          <div className="middle-container">
+            <Bowl
+              ingredientsArr={ingredientsArr}
+              setIngredientsArr={setIngredientsArr}
+            />
+            <Recipes recipesInfoArr={recipesInfoArr} />
+          </div>
+          <div className="right-container">
+            <Favourites />
+          </div>
+        </IngredientProvider>
       </main>
     </div>
   );
