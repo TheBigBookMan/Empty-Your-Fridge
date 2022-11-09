5ae43c035851bfd391c61333a5a4757ada371817
Ben Smerd <bsmerd@adam.com.au>
2022-11-09 11:05:15 +1030

    added in FavouriteProvider to children components

--- empty-your-fridge/src/App.js
+++ empty-your-fridge/src/App.js
@@ -2,15 +2,13 @@ import "./styles/App.css";
 import { useState } from "react";
 import { IngredientProvider } from "./utils/IngredientsContext";
 import { RecipeProvider } from "./utils/RecipesContext";
+import { FavouriteProvider } from "./utils/FavouritesContext";
 import Ingredients from "./components/Ingredients/Ingredients";
 import Bowl from "./components/Bowl/Bowl";
 import Recipes from "./components/Recipes/Recipes";
 import Favourites from "./components/Favourites/Favourites";
 
 function App() {
-  // ! can probably delete the usestate for ingredients because now in global state
-  const [recipesInfoArr, setRecipesInfoArr] = useState([]);
-
   //TODO ingredientsArr needs to be passed into the Recipes component where it can be added to local storage and searched in the fetch call
 
   // TODO Create a state array that holds the selection criteria of cuisine, meal, health and pass props into ingredients to get values
@@ -24,17 +22,17 @@ function App() {
       <main className="container">
         <IngredientProvider>
           <RecipeProvider>
-            <Ingredients
-              recipesInfoArr={recipesInfoArr}
-              setRecipesInfoArr={setRecipesInfoArr}
-            />
-            <div className="middle-container">
-              <Bowl />
-              <Recipes recipesInfoArr={recipesInfoArr} />
-            </div>
-            <div className="right-container">
-              <Favourites />
-            </div>
+            <Ingredients />
+            <FavouriteProvider>
+              <div className="middle-container">
+                <Bowl />
+
+                <Recipes />
+              </div>
+              <div className="right-container">
+                <Favourites />
+              </div>
+            </FavouriteProvider>
           </RecipeProvider>
         </IngredientProvider>
       </main>
