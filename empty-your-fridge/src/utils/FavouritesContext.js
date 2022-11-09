31745cce1e042dc7973ce834454c5d20b03011c6
Ben Smerd <bsmerd@adam.com.au>
2022-11-09 11:03:49 +1030

    created favourites context

---
+++ empty-your-fridge/src/utils/FavouritesContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from "react";
+
+const FavouriteContext = createContext();
+
+export const useFavouriteContext = () => useContext(FavouriteContext);
+
+export const FavouriteProvider = ({ children }) => {
+  const [favourites, setFavourites] = useState([]);
+
+  const addFavourite = (favourite) => {
+    setFavourites([...favourites, favourite]);
+  };
+
+  const removeFavourite = (input) => {
+    const newFavouritesList = favourites.filter(
+      (favourite) => favourite !== input
+    );
+    setFavourites(newFavouritesList);
+  };
+
+  return (
+    <FavouriteContext.Provider
+      value={{ favourites, addFavourite, removeFavourite }}
+    >
+      {children}
+    </FavouriteContext.Provider>
+  );
+};
