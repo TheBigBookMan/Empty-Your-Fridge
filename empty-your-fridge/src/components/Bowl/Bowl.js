ac79cf8f129a6ce563fa5df0e7e60abd370121c6
Ben Smerd <bsmerd@adam.com.au>
2022-11-16 17:23:25 +1030

    added in design layout for the bowl

--- empty-your-fridge/src/components/Bowl/Bowl.js
+++ empty-your-fridge/src/components/Bowl/Bowl.js
@@ -3,22 +3,22 @@ import { useIngredientContext } from "../../utils/IngredientsContext";
 
 //TODO need to make the X buttons highlight red on hover and then onClick will remove the ingredients from the ingredients arr
 
+// TODO change the fontawesome icon into the react icons
+
 const Bowl = () => {
   const { ingredients, removeIngredient } = useIngredientContext();
   return (
-    <div>
-      <ul>
-        {ingredients.map((ingredient) => (
-          <li key={ingredient} value={ingredient}>
-            <p>{ingredient.toLowerCase()}</p>{" "}
-            <i
-              className="fa-regular fa-circle-xmark cross"
-              onClick={() => removeIngredient(ingredient)}
-            ></i>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <ul className="flex flex-row flex-wrap w-full gap-10 border-solid border-2 border-slate-200 rounded-md bg-slate-100 shadow-md p-2 items-center justify-around">
+      {ingredients.map((ingredient) => (
+        <li key={ingredient} value={ingredient}>
+          <p>{ingredient.toLowerCase()}</p>{" "}
+          <i
+            className="fa-regular fa-circle-xmark cross"
+            onClick={() => removeIngredient(ingredient)}
+          ></i>
+        </li>
+      ))}
+    </ul>
   );
 };
 
