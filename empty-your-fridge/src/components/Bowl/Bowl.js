9174a37ef53e8d50a7d5c405fdc30e1988696f3e
Ben Smerd <bsmerd@adam.com.au>
2022-11-16 20:20:21 +1030

    added conditional rendering for the bowl based on ingredient array

--- empty-your-fridge/src/components/Bowl/Bowl.js
+++ empty-your-fridge/src/components/Bowl/Bowl.js
@@ -7,24 +7,29 @@ import { useIngredientContext } from "../../utils/IngredientsContext";
 
 const Bowl = () => {
   const { ingredients, removeIngredient } = useIngredientContext();
+  console.log(ingredients);
   return (
     <div className="flex flex-col gap-2 border-solid border-2 border-slate-200 rounded-md bg-slate-100 shadow-md p-2">
-      <p className="text-center font-bold text-lg">Ingredient Bowl</p>
-      <ul className="flex flex-row flex-wrap w-full gap-5  items-center justify-around">
-        {ingredients.map((ingredient) => (
-          <li
-            key={ingredient}
-            value={ingredient}
-            className="flex flex-col items-center"
-          >
-            <p className="text-indigo-600">{ingredient.toLowerCase()}</p>{" "}
-            <i
-              className="fa-regular fa-circle-xmark cross"
-              onClick={() => removeIngredient(ingredient)}
-            ></i>
-          </li>
-        ))}
-      </ul>
+      <h1 className="text-center font-bold text-lg">Ingredient Bowl</h1>
+      {ingredients.length === 0 ? (
+        <p>Add in ingredients above to be added to the ingredient bowl.</p>
+      ) : (
+        <ul className="flex flex-row flex-wrap w-full gap-5  items-center justify-around">
+          {ingredients.map((ingredient) => (
+            <li
+              key={ingredient}
+              value={ingredient}
+              className="flex flex-col items-center"
+            >
+              <p className="text-indigo-600">{ingredient.toLowerCase()}</p>{" "}
+              <i
+                className="fa-regular fa-circle-xmark cross"
+                onClick={() => removeIngredient(ingredient)}
+              ></i>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
